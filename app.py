356a7aeb1ea83a518c8c0ba33035b202be1092f7
thanu-12345-r <thanmairayapudi26@gmail.com>
2026-05-14 22:04:53 +0530

    fix mysql database selection

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_db():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT",3306)),
+        port=int(os.getenv("MYSQLPORT")),
         cursorclass=pymysql.cursors.Cursor
     )
 # ---------------- HOME PAGE ----------------
