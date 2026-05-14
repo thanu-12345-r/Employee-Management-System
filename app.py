e18c7a2a8bed931941cfa0174bb7e7c4a617f0f0
thanu-12345-r <thanmairayapudi26@gmail.com>
2026-05-14 11:46:20 +0530

    Railway port fix

--- app.py
+++ app.py
@@ -310,7 +310,9 @@ def logout():
 
 # ---------------- RUN SERVER ----------------
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
     
